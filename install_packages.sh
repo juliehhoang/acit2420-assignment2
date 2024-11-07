8d712728efc230d98e691476ae969a01d5b62235
juliehhoang <jjuliehhoang@gmail.com>
2024-11-06 18:09:30 -0800

    edited installed_packages.sh and user defined list of packages in a txt file instead of hardcored in the script.

--- install_packages.sh
+++ install_packages.sh
@@ -4,28 +4,31 @@
 # This script checks if it is run with root privileges, verifies if each package
 # is already installed, and installs it if necessary.
 
-# Define the list of packages to install
-PACKAGES=("kakoune" "tmux")
+# Define the file that contains the user-defined list of packages
+PACKAGES="packages.txt"
 
 # Check if the script is being run as root.
 # $EUID is the effective user ID of the current user.
 # If the user is not root, the script displays a message and exits.
 # Reference: https://stackoverflow.com/questions/18215973/how-to-check-if-running-as-root-in-a-bash-script
 if [[ $EUID -ne 0 ]]; then
-    # Inform the user that the script needs to be run as root
-    echo "Please try using 'sudo'. This script needs to be run as root."
-    exit 1  # Exit with a status of 1 to indicate an error
+    echo "This script needs to be run as root (use sudo)."
+    exit 1  # Exit status of 1 to indicate an error
 fi
 
-# Loop through each package in the list to check if it's installed
-for package in "${PACKAGES[@]}"; do
-    # Use pacman -Qi to check if the package is already installed
-    # It will return a non-zero exit code if the package is not installed
+# Check if the package file exists
+if [[ ! -f "$PACKAGES" ]]; then
+    echo "Package file '$PACKAGES' not found. Please create a file with a list of package names."
+    exit 1  # Exit with an error code if the file does not exist
+fi
+
+# Read the package list from the file and install each package
+for package in $(cat "$PACKAGES"); do
+    # Check if the package is already installed
     if ! pacman -Qi "$package"; then
         echo "Installing $package..."
         
-        # Install the package using pacman, Arch Linux's package manager
-        # -S option installs the specified packages
+        # Install the package using pacman
         if pacman -S "$package"; then
             echo "$package installed successfully."
         else
@@ -36,5 +39,4 @@ for package in "${PACKAGES[@]}"; do
         echo "$package is already installed."
     fi
 done
-
 
